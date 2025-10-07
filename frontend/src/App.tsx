07aaa70603a353e42b3f49a65beabffc0234b73b
Rodrigo Gargani Oliveira <mjgargani@gmail.com>
2025-10-07 10:07:49 -0300

    feat: trying to define a theme state

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,17 +5,24 @@ import Container from './components/templates/Container';
 
 import './App.css';
 import { useLocalStorage } from './hooks/useLocalStorage';
+import { useEffect, useState } from 'react';
 
 
 function App() {
   const title = import.meta.env.VITE_COMPANY_NAME ? `Nexum: ${import.meta.env.VITE_COMPANY_NAME}` : 'Nexum: Mecânica SHALLON';
   useTitle(title);
+  const [theme, setTheme] = useState<string>('dark');
 
-  const [get, set] = useLocalStorage();
+  const localStorage = useLocalStorage();
+
+  useEffect(() => {
+    localStorage.getItem('theme', 'dark');
+    setTheme(localStorage.getItem('theme', 'dark') ?? 'dark');
+  }, [localStorage])
 
   return (
     <HashRouter>
-      <div className="dark:bg-[var(--bg)]">
+      <div className={`${theme} bg-[var(--bg)] min-h-screen flex flex-col`}>
         <main className="">
           <Container landing={location.hash === '#/' || location.hash === ''} />
         </main>
