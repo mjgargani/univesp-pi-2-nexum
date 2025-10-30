b86f577122f31d3da3d2e33d703cfc280a9ab413
Rodrigo Gargani Oliveira <mjgargani@gmail.com>
2025-10-30 14:17:02 -0300

    feat: user controller role guard

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,18 +1,24 @@
-import { Controller, Get, Post, Put, Delete, Body, Param, HttpException, HttpStatus } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param, HttpException, HttpStatus, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
 
 @Controller('customers')
 export class UsersController {
   constructor(private readonly UsersService: UsersService) {}
 
   @Get()
+  @Roles('admin')
+  @UseGuards(RolesGuard)
   findAll() {
     return this.UsersService.findAll();
   }
 
   @Get(':id')
+  @Roles('admin')
+  @UseGuards(RolesGuard)
   findOne(@Param('id') id: string) {
     const customer = this.UsersService.findOne(id);
     if (!customer) {
@@ -21,12 +27,27 @@ export class UsersController {
     return customer;
   }
 
+  @Get('profile/:id')
+  @Roles('admin', 'user')
+  @UseGuards(RolesGuard)
+  getProfile(@Param('id') id: string) {
+    const customer = this.UsersService.findOne(id);
+    if (!customer) {
+      throw new HttpException('Usuário não encontrado', HttpStatus.NOT_FOUND);
+    }
+    return customer;
+  }
+
   @Post()
+  @Roles('admin')
+  @UseGuards(RolesGuard)
   create(@Body() createUserDto: CreateUserDto) {
     return this.UsersService.create(createUserDto);
   }
 
   @Put(':id')
+  @Roles('admin')
+  @UseGuards(RolesGuard)
   update(@Param('id') id: string, @Body() dto: UpdateUserDto) {
     const customer = this.UsersService.update(id,dto);
     if (!customer) {
@@ -36,6 +57,8 @@ export class UsersController {
   }
 
   @Delete(':id')
+  @Roles('admin')
+  @UseGuards(RolesGuard)
   remove(@Param('id') id: string) {
     const success = this.UsersService.remove(id);
     if (!success) {
